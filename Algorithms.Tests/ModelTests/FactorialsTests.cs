829cef6c113231f89e1d3e01ce578025cc5b1ca4
Richard <charichard09@gmail.com>
2023-07-15 09:59:18 -0700

    add code and tests to pass

--- Algorithms.Tests/ModelTests/FactorialsTests.cs
+++ Algorithms.Tests/ModelTests/FactorialsTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Algorithms.Models;
+using System;
 
 namespace Algorithms.Tests;
 
@@ -7,13 +8,64 @@ namespace Algorithms.Tests;
 public class FactorialTests
 {
   [TestMethod]
-  public void Calculate_ReturnZero()
+  public void Calculate_NIsTwo_ReturnTwo()
   {
     // Arrange
 
     // Act
+    int result = Factorials.Calculate(2);
 
     // Assert
-    Assert.AreEqual(Factorials.Calculate(), 0);
+    Assert.AreEqual(result, 2);
+  }
+
+  [TestMethod]
+  public void Calculate_WhenNIsOutOfRangeAboveTwelve_ThrowOutOfRangeException()
+  {
+    // Arrange
+    int n = 13;
+
+    // Act
+
+    // Assert
+    Assert.ThrowsException<ArgumentOutOfRangeException>(() => Factorials.Calculate(n));
+  }
+
+  [TestMethod]
+  public void Calculate_WhenNIsOutOfRangeBelowZero_ThrowOutOfRangeException()
+  {
+    // Arrange
+    int n = -1;
+
+    // Act
+
+    // Assert
+    Assert.ThrowsException<ArgumentOutOfRangeException>(() => Factorials.Calculate(n));
+  }
+
+  [TestMethod]
+  public void Calculate_WhenNIsFive_OneHundredTwenty()
+  {
+    // Arrange
+    int n = 5;
+
+    // Act
+    int result = Factorials.Calculate(n);
+
+    // Assert
+    Assert.AreEqual(result, 120);
+  }
+
+  [TestMethod]
+  public void Calculate_WhenNIsZero_One()
+  {
+    // Arrange
+    int n = 0;
+
+    // Act
+    int result = Factorials.Calculate(n);
+
+    // Assert
+    Assert.AreEqual(result, 1);
   }
 }
--- Algorithms/Models/Factorials.cs
+++ Algorithms/Models/Factorials.cs
@@ -1,21 +1,39 @@
 // A factorial is the product of numbers from 1 to n
 // given int n, return the factorial of n
 // if n is > 12 or < 0, throw an ArgumentOutOfRangeException
+using System;
 
 namespace Algorithms.Models;
 
 public class Factorials
 {
-  public static int Calculate()
+  public static int Calculate(int n)
   {
     // if n == 2, return 2
+    if (n == 2)
+    {
+      return 2;
+    } 
     // if n is > 12 or < 0, throw an ArgumentOutOfRangeException
+    else if (n > 12 || n < 0)
+    {
+      throw new ArgumentOutOfRangeException(nameof(n), "Argument Out of Range, less than 0, or greater than 12");
+    }
+    else if (n == 0 || n == 1)
+    {
+      return 1;
+    }
 
-    // for loop n times, start i at 2
     // declare int sum = 2;
-    // for each iteration, sum = sum * i 
+    int sum = 2;
+    // for loop n times, start i at 3
+    for (int i = 3; i <= n; i++)
+    {
+      // for each iteration, sum = sum * i 
+      sum = sum * i;
+    }
 
     // return sum
-    return 0;
+    return sum;
   }
 }
