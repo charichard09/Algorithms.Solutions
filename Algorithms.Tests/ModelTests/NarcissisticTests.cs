d78801eb8c36e3ef2b20f13c87d5285241b7a420
Richard <charichard09@gmail.com>
2023-07-28 19:02:01 -0700

    finish coding logic to pass tests

--- Algorithms.Tests/ModelTests/NarcissisticTests.cs
+++ Algorithms.Tests/ModelTests/NarcissisticTests.cs
@@ -4,17 +4,41 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Algorithms.Tests;
 
 [TestClass]
-class NarcissisticTests
+public class NarcissisticTests
 {
   [TestMethod]
   public void Calculate_WhenNIsOne_True()
   {
-    // 
+    // Arrange
 
     // Act
     bool result = Narcissistic.Calculate(1);
 
     // Assert
-    Assert.AreEqual(result, true);
+    Assert.AreEqual(true, result);
+  }
+
+  [TestMethod]
+  public void Calculate_WhenNIsOneHundredFiftyThree_True()
+  {
+    // Arrange
+
+    // Act
+    bool result = Narcissistic.Calculate(153);
+
+    // Assert
+    Assert.AreEqual(true, result);
+  }
+
+  [TestMethod]
+  public void Calculate_WhenNIs1652_False()
+  {
+    // Arrange
+
+    // Act
+    bool result = Narcissistic.Calculate(1652);
+
+    // Assert 
+    Assert.AreEqual(false, result);
   }
 }
